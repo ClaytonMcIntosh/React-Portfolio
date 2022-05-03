6add20a3f81299735393c08bb6355a208471a676
Clay <claymcintosh@gmail.com>
2022-05-04 08:53:54 +1000

    Added simple menu items components

---
+++ src/components/Menuitem.js
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types'
+
+const Menuitem = ({text}) => {
+  return (
+    <div className="menu-item">
+      {text}
+    </div>
+  )
+}
+
+export default Menuitem
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import Menuitem from './Menuitem';
 
 const Navbar = () => {
     return (
         <nav className='main-nav'>
             <h1> Clayton McIntosh </h1>
-            <Link to="/About">About </Link>
-            <Link to="/Portfolio">Portfolio </Link>
-            <Link to="/Contact">Contact </Link>
-            <Link to="/Resume">Resume </Link>
+            <Link to="/About"> <Menuitem text='About' /> </Link>
+            <Link to="/Portfolio"> <Menuitem text='Portfolio' /></Link>
+            <Link to="/Contact"> <Menuitem text='Contact' /> </Link>
+            <Link to="/Resume"> <Menuitem text='Resume' /> </Link>
+
         </nav>
     )
 }
