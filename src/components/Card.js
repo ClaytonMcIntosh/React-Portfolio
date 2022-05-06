ff0a86f657f366fbd1466d960267fca448424afc
Clay <claymcintosh@gmail.com>
2022-05-06 10:27:12 +1000

    flex box sitting nicely

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,8 +4,8 @@ const Card = ({ id, img, heading, text }) => {
     return (
             <div className='card'>
                 <img className="small-img" src={img} alt="" />
-                <h1> {heading} </h1>
-                <p> {text} </p>
+                {/* <h1> {heading} </h1>
+                <p> {text} </p> */}
             </div>
     )
 }
