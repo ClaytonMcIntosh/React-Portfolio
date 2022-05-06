722827b3ce75b17635ec36e3cf730c332012510b
Clay <claymcintosh@gmail.com>
2022-05-07 07:59:44 +1000

    Basic page setup

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -9,7 +9,6 @@ const style = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: '80%',
     bgcolor: 'background.paper',
     boxShadow: 24,
     p: 2,
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,15 +3,17 @@ import React from "react"
 function Contact(props) {
     return (
         <div className="page">
-            <h1 className="page-headings">Contact</h1>
-            <p className="info">Fill in the form below to send Clayton a message:</p>
-            <form className="contact-form">
-                <label >Name:</label><br />
-                <input type="text" id="name" name="name" /> <br />
-                <label for="message">Last name:</label> <br />
-                <textarea name="message" rows="10" cols="30" /> <br />
-                <input type="submit" value="Submit" /> <br />
-            </form>
+            <div className="contact-page">
+                <h1 className="page-headings">Contact</h1>
+                <form className="contact-form">
+                <p>Fill in the form below to send Clayton a message:</p>
+                    <label >Name:</label><br />
+                    <input type="text" id="name" name="name" /> <br />
+                    <label for="message">Last name:</label> <br />
+                    <textarea name="message" rows="10" cols="30" /> <br />
+                    <input type="submit" value="Submit" /> <br />
+                </form>
+            </div>
         </div>
     )
 }
