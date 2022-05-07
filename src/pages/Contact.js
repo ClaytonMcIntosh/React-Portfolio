b4cdeeb60a4f60457ebac9da8e253c8605137bd4
Clay <claymcintosh@gmail.com>
2022-05-07 14:32:43 +1000

    working on the form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,15 +6,17 @@ function Contact(props) {
             <div className="contact-page">
                 <h1 className="page-headings">Contact</h1>
                 <form className="contact-form">
-                <p>Fill in the form below to send Clayton a message:</p>
+                    <p>Fill in the form below to send Clayton a message:</p>
                     <label >Name:</label><br />
-                    <input type="text" id="name" name="name" /> <br />
-                    <label for="message">Last name:</label> <br />
-                    <textarea name="message" rows="10" cols="30" /> <br />
+                    <input className="form-el" type="text" id="name" required placeholder="Field required" /> <br />
+                    <label className="form-el" for="message">Email:</label> <br />
+                    <label for="email">Enter your email:</label>
+                    <input type="email" id="email" pattern='[a-z0-9]+@[a-z]+\.[a-z]{2,3}' size="30" required /> <br />
+                    <textarea name="message" rows="10" cols="30" required placeholder="Field required" /> <br />
                     <input type="submit" value="Submit" /> <br />
                 </form>
             </div>
-        </div>
+        </div >
     )
 }
 
